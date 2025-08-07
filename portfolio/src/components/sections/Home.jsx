1710c597cd51e2b287f64b1491076435978a3106
Akanksha97 <akankshayadav8467@gmail.com>
2025-08-07 18:09:41 +0530

    home

--- portfolio/src/components/sections/Home.jsx
+++ portfolio/src/components/sections/Home.jsx
@@ -7,6 +7,15 @@ export const Home =()=>{
         >
             <div className="text-center z-10 px-4">
                 <h1 className="text-5xl md:text-7xl font-old mb-6 bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent leading-right">  Hi,I'm Akanksha Yadav</h1>
+                <p className="text-gray-400 text-lg mb-8 max-w-lg mx-auto">
+                    I am a full stack developer with a passion for creating dynamic and responsive web applications. I love to explore new technologies and continuously improve my skills.
+                </p>
+                <div className="flex justity-center  space-x-4">
+                    <a  href="#projects" className=" bg-blue-500 text-white px-6 py-3 rounded-lg shadow-lg hover:bg-blue-600 transition-colors duration-300">
+vew projects
+                    </a>
+
+                </div>
             </div>
 
         </section>
